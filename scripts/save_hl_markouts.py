e4a23144ac7791330379223ad4717d8e58db2757
rohanshrothrium <shrothriumrohan@gmail.com>
2026-04-14 19:26:59 +0530

    fix: chunked queries to prevent timeouts, feature: allow --from/--to timestamp to analyze markouts at in a more granular fashion
    Signed-off-by: rohanshrothrium <shrothriumrohan@gmail.com>

--- scripts/save_hl_markouts.py
+++ scripts/save_hl_markouts.py
@@ -10,13 +10,19 @@ from lib.trading import compute_markouts, print_trading_report
 
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), '../data')
 
-def get_vault_markouts(date, market='MONUSDC', report=False):
+def get_vault_markouts(date, market='MONUSDC', report=False, start=None, end=None):
     date_str = date.strftime('%Y%m%d')
     strategy_state = pd.read_csv(f'{OUTPUT_DIR}/{market}_vault_strategy_state_{date_str}.csv')
     hl_fills = pd.read_csv(f'{OUTPUT_DIR}/hl_fills_{date_str}.csv')
     hl_fills['is_bid'] = hl_fills['side'] == 'buy'
     hl_fills['size_usd'] = hl_fills['price'] * hl_fills['size']
     strategy_state['time'] = pd.to_datetime(strategy_state['time'], format='mixed')
+    hl_fills['time'] = pd.to_datetime(hl_fills['time'], format='mixed').dt.tz_localize(None)
+
+    if start is not None and end is not None:
+        hl_fills = hl_fills[(hl_fills['time'] >= start) & (hl_fills['time'] < end)]
+        strategy_state = strategy_state[(strategy_state['time'] >= start) & (strategy_state['time'] < end)]
+
     hl_markouts = compute_markouts(hl_fills, strategy_state.set_index('time')['fair_value'])
     if report:
         print_trading_report(
@@ -28,20 +34,29 @@ def get_vault_markouts(date, market='MONUSDC', report=False):
     return hl_markouts
 
 
-if __name__  == '__main__':
-    if len(sys.argv) == 1:
-        print('Usage: python vault_trading_summary.py <yyyymmdd> [market]')
-        print('If market is not provided, it will default to MONUSDC')
-        sys.exit(1)
-    elif len(sys.argv) == 2:
-        date = pd.Timestamp(sys.argv[1])
-        date_str = date.strftime('%Y%m%d')
-        mos_fair_value = get_vault_markouts(date, market='MONUSDC', report=True)
-        mos_fair_value.to_csv(f'{OUTPUT_DIR}/hl_markouts_{date_str}.csv', index=False)
-    elif len(sys.argv) == 3:
-        date = pd.Timestamp(sys.argv[1])
-        date_str = date.strftime('%Y%m%d')
-        market = sys.argv[2]
-        mos_fair_value = get_vault_markouts(date, market=market)
-        print(mos_fair_value['mo_fair_value_5s'].describe())
-        mos_fair_value.to_csv(f'{OUTPUT_DIR}/hl_markouts_{date_str}.csv', index=False)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Compute HL fill markout analysis.')
+    parser.add_argument('--date', metavar='YYYYMMDD', help='Run analysis for a full day, e.g. 20250411')
+    parser.add_argument('--from', dest='from_ts', metavar='TIMESTAMP', help='Start timestamp, e.g. "2025-04-11 08:00:00"')
+    parser.add_argument('--to', dest='to_ts', metavar='TIMESTAMP', help='End timestamp, e.g. "2025-04-11 16:00:00"')
+    parser.add_argument('--market', default='MONUSDC', help='Market (default: MONUSDC)')
+    args = parser.parse_args()
+
+    if args.date and (args.from_ts or args.to_ts):
+        parser.error('Provide either --date or --from/--to, not both.')
+    if not args.date and not (args.from_ts and args.to_ts):
+        parser.error('Provide either --date or both --from and --to.')
+
+    if args.date:
+        start = pd.Timestamp(args.date)
+        filter_start, filter_end = None, None
+    else:
+        start = pd.Timestamp(args.from_ts)
+        end = pd.Timestamp(args.to_ts)
+        filter_start, filter_end = start, end
+
+    date_str = start.strftime('%Y%m%d')
+    mos_fair_value = get_vault_markouts(start, market=args.market, report=True, start=filter_start, end=filter_end)
+    print(mos_fair_value['mo_fair_value_5s'].describe())
+    mos_fair_value.to_csv(f'{OUTPUT_DIR}/hl_markouts_{date_str}.csv', index=False)
